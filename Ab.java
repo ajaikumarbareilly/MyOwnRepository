f9579d179bd8162752a0b055fbfebc829265ee7b
nazirmohd2006 <nazirmohd2006@gmail.com>
2018-05-21 10:42:25 +0530

    Update Ab.java

--- Ab.java
+++ Ab.java
@@ -7,6 +7,7 @@ class Ab extends Showable
  public void invoke()
  {
   System.out.println("Ok, I am implemented now man! Don't worry about it.");
+  System.out.println("I have changed this file in github.");
  }
  public static void main(String arr[])
  {
